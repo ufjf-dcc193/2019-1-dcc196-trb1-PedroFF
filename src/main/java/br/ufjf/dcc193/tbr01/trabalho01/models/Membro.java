b2a7d74818a37378ca8b2661ba41e44510608adb
Pedro Freitas <soufreitas.pedro@gmail.com>
2019-04-11 00:38:58 -0300

    Criando membro

--- src/main/java/br/ufjf/dcc193/tbr01/trabalho01/models/Membro.java
+++ src/main/java/br/ufjf/dcc193/tbr01/trabalho01/models/Membro.java
@@ -1,4 +1,9 @@
 package br.ufjf.dcc193.tbr01.trabalho01.models;
 
 public class Membro {
+    private String nome;
+    private String funcao;
+    private String email;
+    private String dataEntrada;
+    private String dataSaida;
 }
