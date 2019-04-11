9c94acd39f6666777fadbb621fa96577ddcd1579
Pedro Freitas <soufreitas.pedro@gmail.com>
2019-04-10 22:26:51 -0300

    Inicializando projeto do trabalho 01

---
+++ src/main/java/br/ufjf/dcc193/tbr01/trabalho01/Trabalho01Application.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.tbr01.trabalho01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trabalho01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Trabalho01Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/ufjf/dcc193/tbr01/trabalho01/Trabalho01ApplicationTests.java
@@ -0,0 +1,16 @@
+package br.ufjf.dcc193.tbr01.trabalho01;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Trabalho01ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
