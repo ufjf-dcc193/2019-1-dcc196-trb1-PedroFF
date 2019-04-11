e38bbf94629ac78cbfe2e7ecc9551ea5233eabb7
Pedro Freitas <soufreitas.pedro@gmail.com>
2019-04-11 00:39:37 -0300

    Adicionando configuração

--- src/main/java/br/ufjf/dcc193/tbr01/trabalho01/HomeController.java
+++ src/main/java/br/ufjf/dcc193/tbr01/trabalho01/HomeController.java
@@ -1,4 +1,13 @@
 package br.ufjf.dcc193.tbr01.trabalho01;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
+
+    @RequestMapping({"", "index.html"})
+    public String home(){
+        return "home";
+    }
 }
