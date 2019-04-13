25c46a5534b1d8bec614126c9939156685d5f457
Pedro Freitas <soufreitas.pedro@gmail.com>
2019-04-12 22:37:37 -0300

    Criando classe atividade

--- src/main/java/br/ufjf/dcc193/tbr01/trabalho01/models/Atividade.java
+++ src/main/java/br/ufjf/dcc193/tbr01/trabalho01/models/Atividade.java
@@ -1,4 +1,78 @@
 package br.ufjf.dcc193.tbr01.trabalho01.models;
 
+import java.util.Date;
+
 public class Atividade {
+    private String titulo;
+    private String descricao;
+    private Date dataIni;
+    private Date dataFim;
+    private int totalHorasAssistencial;
+    private int totalHorasJuridica;
+    private int totalHorasExecutiva;
+    private int totalHorasFinanceira;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getDataIni() {
+        return dataIni;
+    }
+
+    public void setDataIni(Date dataIni) {
+        this.dataIni = dataIni;
+    }
+
+    public Date getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(Date dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public int getTotalHorasAssistencial() {
+        return totalHorasAssistencial;
+    }
+
+    public void setTotalHorasAssistencial(int totalHorasAssistencial) {
+        this.totalHorasAssistencial = totalHorasAssistencial;
+    }
+
+    public int getTotalHorasJuridica() {
+        return totalHorasJuridica;
+    }
+
+    public void setTotalHorasJuridica(int totalHorasJuridica) {
+        this.totalHorasJuridica = totalHorasJuridica;
+    }
+
+    public int getTotalHorasExecutiva() {
+        return totalHorasExecutiva;
+    }
+
+    public void setTotalHorasExecutiva(int totalHorasExecutiva) {
+        this.totalHorasExecutiva = totalHorasExecutiva;
+    }
+
+    public int getTotalHorasFinanceira() {
+        return totalHorasFinanceira;
+    }
+
+    public void setTotalHorasFinanceira(int totalHorasFinanceira) {
+        this.totalHorasFinanceira = totalHorasFinanceira;
+    }
 }
--- src/main/java/br/ufjf/dcc193/tbr01/trabalho01/models/Membro.java
+++ src/main/java/br/ufjf/dcc193/tbr01/trabalho01/models/Membro.java
@@ -1,9 +1,67 @@
 package br.ufjf.dcc193.tbr01.trabalho01.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Membro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
     private String nome;
     private String funcao;
     private String email;
     private String dataEntrada;
     private String dataSaida;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDataEntrada() {
+        return dataEntrada;
+    }
+
+    public void setDataEntrada(String dataEntrada) {
+        this.dataEntrada = dataEntrada;
+    }
+
+    public String getDataSaida() {
+        return dataSaida;
+    }
+
+    public void setDataSaida(String dataSaida) {
+        this.dataSaida = dataSaida;
+    }
 }
