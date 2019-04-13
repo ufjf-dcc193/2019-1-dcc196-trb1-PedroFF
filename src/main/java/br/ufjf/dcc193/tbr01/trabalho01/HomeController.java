b31d0f14d1e126b8bd36732a67789eec64f7bdc3
Pedro Freitas <soufreitas.pedro@gmail.com>
2019-04-13 15:11:54 -0300

    Criando mapeamento para listar as entidades

--- src/main/java/br/ufjf/dcc193/tbr01/trabalho01/HomeController.java
+++ src/main/java/br/ufjf/dcc193/tbr01/trabalho01/HomeController.java
@@ -1,28 +1,55 @@
 package br.ufjf.dcc193.tbr01.trabalho01;
 
+import br.ufjf.dcc193.tbr01.trabalho01.models.Sede;
+import br.ufjf.dcc193.tbr01.trabalho01.repositories.AtividadeRepository;
+import br.ufjf.dcc193.tbr01.trabalho01.repositories.MembroRepository;
+import br.ufjf.dcc193.tbr01.trabalho01.repositories.SedeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private SedeRepository repSede;
+    @Autowired
+    private AtividadeRepository repAtividade;
+    @Autowired
+    private MembroRepository repMembro;
+
     @RequestMapping({"", "index.html"})
-    public String home(){
+    public String home() {
         return "home";
     }
 
     @RequestMapping("sedes.html")
-    public String sede(){
-        return "sede";
+    public ModelAndView sedes() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("sedes");
+        List<Sede> sedes = repSede.findAll();
+        mv.addObject("sedes", sedes);
+        return mv;
     }
 
     @RequestMapping("atividades.html")
-    public String atividades(){
-        return "atividades";
+    public ModelAndView atividades() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("atividades");
+        List<Sede> atividades = repSede.findAll();
+        mv.addObject("atividades", atividades);
+        return mv;
     }
 
     @RequestMapping("membros.html")
-    public String membros(){
-        return "membros";
+    public ModelAndView membros() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("membros");
+        List<Sede> membros = repSede.findAll();
+        mv.addObject("membros", membros);
+        return mv;
     }
 }
